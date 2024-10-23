9398781b8d2c84b002ab4b5a492d8ee6f2c75a3b
Tatchakorn S <saitako@outlook.com>
2024-10-23 11:13:26 +0700

    chore: structure project

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/BinLab64/Orbix-client/api"
@@ -8,4 +9,17 @@ import (
 
 func main() {
 	fmt.Printf("--- %s", api.DefaultBaseURL)
+	TestApi()
+}
+
+func TestApi() {
+	client := api.NewClient(api.ClientOptions{})
+	res, err := client.NewExchangeInfoService().Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+	}
+	// res.Symbols
+	fmt.Println(res)
+	// client.NewExchangeInfoService()
+
 }
